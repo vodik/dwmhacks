dff0b6d82dad32a5a62f7a7266b694148c394124
Simon Gomizelj <simongmzlj@gmail.com>
2010-03-17 22:23:04 -0400

    fix alignment issues and a bug causing it to fail to manage the last window

--- pidgin.c
+++ pidgin.c
@@ -14,21 +14,23 @@ pidgin(Monitor *m) {
 	/* master */
 	mw = m->mfacts[m->curtag] * m->ww;
 	if(mk) {
-		resize(mk, m->mx + mw, m->my + bh, m->mw - mw - 2 * mk->bw, m->wh - 2 * mk->bw, False);
+		resize(mk, m->mx + mw, m->my + (topbar ? bh : 0), m->mw - mw - 2 * mk->bw, m->wh - 2 * mk->bw - (topbar ? bh : 0), False);
 		strncpy(m->ltsymbol, "==P", sizeof m->ltsymbol);
 		if(--n == 0)
 			return;
 	}
 	/* tile stack */
 	x = m->mx;
-	y = m->my + bh;
+	y = topbar ? m->my + bh : m->my;
 	w = mk ? mw : m->ww;
 	h = m->wh / n;
 	if(h < bh)
 		h = m->wh;
 	for(i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
-		if(c == mk)
+		if(c == mk) {
+			--i;
 			continue;
+		}
 		resize(c, x, y, w - 2 * c->bw,
 		       ((i + 1 == n) ? m->wy + m->wh - y : h) - 2 * c->bw, False);
 		if(h != m->wh)
